723f8bc345c9017fef22a729369efe48d0f2cf72
Gabriellllla <99075840+Gabriellllla@users.noreply.github.com>
2022-12-19 14:44:23 +0000

    validare

--- 8Regine.cpp
+++ 8Regine.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int n, st[25], nr_sol = 0 , a[25][25];
 
-
-gujgyuj
-
-
+int valid(int k)
+{
+	for (int i = 1; i < k; i++)
+	{
+		if (st[i] == st[k])
+			return 0;
+		if (abs((float)(st[k] - st[i])) == abs((float)(k - i)))
+			return 0;
+	}
+	return 1;
+}
